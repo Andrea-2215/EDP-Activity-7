11a4ba51d493791187228ac64185f45c06a02498
Andrea222 <kazumibomables@gmail.com>
2026-05-15 14:07:17 +0800

    Update clothingstore (5).sql

--- clothingstore (5).sql
+++ clothingstore (5).sql
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `clothingstore`
 --
@@ -60,9 +54,7 @@ CREATE TABLE `activity_log` (
   `ActionTime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `activity_log`
---
+
 
 INSERT INTO `activity_log` (`LogID`, `ActionType`, `TableName`, `RecordID`, `ActionTime`) VALUES
 (1, 'INSERT', 'orders', 11, '2026-05-13 03:18:06'),
